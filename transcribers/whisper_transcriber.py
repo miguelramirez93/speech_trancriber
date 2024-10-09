ce49bdb0a5673209a091bc0b478cbe2989b39f3c
Jose Miguel Ramirez <miguelramirez93@Joses-MacBook-Pro.local>
2024-10-09 08:36:36 -0500

    Add whisper model enum

--- transcribers/whisper_transcriber.py
+++ transcribers/whisper_transcriber.py
@@ -1,12 +1,17 @@
+from enum import Enum
 from transcribers.transcriber import Transcriber
 import whisper
 from libs.io import Writer
 
 
+class Models(Enum):
+    BASE = "base"
+
+
 class WhisperTranscriber(Transcriber):
-    def __init__(self, writer_impl: Writer) -> None:
+    def __init__(self, writer_impl: Writer, model: str = Models.BASE.value) -> None:
         self.__out_writer = writer_impl
-        self.__model = whisper.load_model("base")
+        self.__model = whisper.load_model(model)
 
     def transcribe(self, raw_audio_data):
         result = self.__model.transcribe(raw_audio_data, fp16=False)
